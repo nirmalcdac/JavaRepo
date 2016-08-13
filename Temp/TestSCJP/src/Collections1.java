4f3b2f62b902afef4ce19436c626e49959b7609e
nirmalcdac <nirmal.sharma3500@gmail.com>
2016-08-13 22:09:48 +0530

    Deleted Redundent files

--- Temp/TestSCJP/src/Collections1.java
+++
@@ -1,6 +0,0 @@
-public class Collections1 {
-	public static void main(String[] args) {
-		Collections1 cl1 = new Collections1();
-		System.out.println(cl1);
-	}
-}
--- Temp/TestSCJP/src/StringChars.java
+++
@@ -1,7 +0,0 @@
-public class StringChars {
-	public static void main(String args[]) {
-		char chars[] = { 'a', 'b', 'c' };
-		String s = new String(chars);
-		System.out.println(s);
-	}
-}
